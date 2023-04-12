186343092ba3a54e4a3051f80efbe5ecbb32d10e
Highpreeth <christiansvilanova@gmail.com>
2023-04-12 12:14:45 +0100

    documentation

--- models/playsModel.js
+++ models/playsModel.js
@@ -8,9 +8,10 @@ async function checkEndGame(game) {
 
 class Play {
     // At this moment I do not need to store information so we have no constructor
-    static worldData;
     // Just a to have a way to determine end of game
     static maxNumberTurns = 10;
+    // And the worlds which are created when the server starts
+    static worldData;
     // we consider all verifications were made
 
     // Load in a team for the player or opponents using their default teams
