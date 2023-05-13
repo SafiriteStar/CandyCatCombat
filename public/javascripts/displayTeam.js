f787dd6dd033edff6479ae35d82f8345d18159cd
Highpreeth <christiansvilanova@gmail.com>
2023-05-13 16:46:02 +0100

    added weapons to default team display table

--- public/javascripts/displayTeam.js
+++ public/javascripts/displayTeam.js
@@ -13,7 +13,7 @@ async function removeCatOnClick(teamCatID) {
     await remakeTable();
 }
 
-async function createCellButton(classList, value, onclick, catName, catImagePath) {
+async function createCellButton(classList, value, onclick, catName, catImagePaths) {
     let button = document.createElement('button');
     for (let i = 0; i < classList.length; i++) {
         button.classList.add(classList[i]);
@@ -27,10 +27,12 @@ async function createCellButton(classList, value, onclick, catName, catImagePath
         button.setAttribute('onclick', onclick);
     }
 
-    let catImage = document.createElement('img');
-    catImage.classList.add('defaultCatImage');
-    catImage.src = catImagePath;
-    button.appendChild(catImage);
+    for (let i = 0; i < catImagePaths.length; i++) {
+        let catImage = document.createElement('img');
+        catImage.classList.add('defaultCatImage');
+        catImage.src = catImagePaths[i];
+        button.appendChild(catImage);
+    }
     
     let catText = document.createElement('p');
     catText.classList.add('defaultCatText');
@@ -40,16 +42,7 @@ async function createCellButton(classList, value, onclick, catName, catImagePath
     return button;
 }
 
-async function createTeamCell(cat, baseCats, canRemove) {
-    let catImagePaths = [
-        './assets/VanillaCat/Vanilla_Cat_Base.png',
-        './assets/CandyCornCat/Candy_Corn_Cat_Base.png',
-        './assets/MawbreakerCat/Mawbreaker_Cat_Base.png',
-        './assets/GumCat/Gum_Cat_Base.png',
-        './assets/PopCandyCat/Pop_Candy_Cat_Base.png',
-        './assets/CaramelCat/Caramel_Cat_Base.png',
-        './assets/ChocoDairyMilkCat/Choco_Dairy_Milk_Cat_Base.png',
-    ];
+async function createTeamCell(cat, baseCats, catImagePaths) {
 
     let tableCell = document.createElement('td');
     tableCell.classList.add('defaultTeamCell');
@@ -69,7 +62,7 @@ async function createTeamCell(cat, baseCats, canRemove) {
     }
 
     // Create a button to remove the currently selected cat
-    let catRemoveButton = await createCellButton(['defaultCatOption'], null, `removeCatOnClick(${cat.id})`, `Remove Cat`, `./assets/UI/CrossOption.png`);
+    let catRemoveButton = await createCellButton(['defaultCatOption'], null, `removeCatOnClick(${cat.id})`, `Remove Cat`, [`./assets/UI/CrossOption.png`]);
     dropdownContent.appendChild(catRemoveButton);
 
     dropdown.appendChild(dropdownContent);
@@ -79,16 +72,7 @@ async function createTeamCell(cat, baseCats, canRemove) {
     return tableCell;
 }
 
-async function createAddCatCell(baseCats) {
-    let catImagePaths = [
-        './assets/VanillaCat/Vanilla_Cat_Base.png',
-        './assets/CandyCornCat/Candy_Corn_Cat_Base.png',
-        './assets/MawbreakerCat/Mawbreaker_Cat_Base.png',
-        './assets/GumCat/Gum_Cat_Base.png',
-        './assets/PopCandyCat/Pop_Candy_Cat_Base.png',
-        './assets/CaramelCat/Caramel_Cat_Base.png',
-        './assets/ChocoDairyMilkCat/Choco_Dairy_Milk_Cat_Base.png',
-    ];
+async function createAddCatCell(baseCats, catImagePaths) {
 
     let tableCell = document.createElement('td');
     tableCell.classList.add('defaultTeamCell');
@@ -96,7 +80,7 @@ async function createAddCatCell(baseCats) {
     let dropdown = document.createElement('div');
     dropdown.classList.add('defaultCatDropdown');
     
-    let addButton = await createCellButton(['defaultCatOption'], null, null, `Add Cat`, `./assets/UI/AddOption.png`);
+    let addButton = await createCellButton(['defaultCatOption'], null, null, `Add Cat`, [`./assets/UI/AddOption.png`]);
     dropdown.appendChild(addButton);
 
     // Drop down that shows all cats
@@ -116,6 +100,16 @@ async function createAddCatCell(baseCats) {
 }
 
 async function createTeamDropDown(team, baseCats) {
+    let catImagePaths = [
+        ['./assets/VanillaCat/Peppermint_Shield.png', './assets/VanillaCat/Vanilla_Cat_Base.png', './assets/VanillaCat/Candy_Cane_Sword.png'],
+        ['./assets/CandyCornCat/Candy_Corn_Cat_Base.png', './assets/CandyCornCat/Chocolate_and_Strawberry_Bow.png'],
+        ['./assets/MawbreakerCat/Mawbreaker_Cat_Base.png', './assets/MawbreakerCat/Candy_Axe_NR.png'],
+        ['./assets/GumCat/Gum_Cat_Base.png', './assets/GumCat/Daggers.png'],
+        ['./assets/PopCandyCat/Pop_Candy_Cat_Base.png', './assets/PopCandyCat/Pop_Rocks.png'],
+        ['./assets/CaramelCat/Caramel_Cat_Base.png', './assets/CaramelCat/Sticky_Caramel.png'],
+        ['./assets/ChocoDairyMilkCat/Choco_Dairy_Milk_Cat_Base.png', './assets/ChocoDairyMilkCat/Healing_Kit.png'],
+    ];
+
     // Create the table
     let table = document.createElement('table');
     table.classList.add('defaultTeamTable');
@@ -128,14 +122,14 @@ async function createTeamDropDown(team, baseCats) {
     // For cat in the default team
     for (let i = 0; i < team.length; i++) {
         // Make a cell for that cat
-        let catCell = await createTeamCell(team[i], baseCats);
+        let catCell = await createTeamCell(team[i], baseCats, catImagePaths);
         // And add it to the row
         tableRow.appendChild(catCell);
     }
 
     // For each cat below 6
     for (let i = 0; i < 6 - team.length; i++) {
-        let addCatCell = await createAddCatCell(baseCats);
+        let addCatCell = await createAddCatCell(baseCats, catImagePaths);
         tableRow.appendChild(addCatCell);
     }
 
