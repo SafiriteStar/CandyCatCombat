f02b0451ffda02b4f4b56f2370db6a6029dca101
Highpreeth <christiansvilanova@gmail.com>
2023-05-14 22:21:50 +0100

    improved tile highlight and team display

--- public/javascripts/ui/renderers/Tile.js
+++ public/javascripts/ui/renderers/Tile.js
@@ -44,6 +44,20 @@ class Tile {
         endShape(CLOSE);
     }
 
+    static drawScaledTile(x, y, scaler) {
+        // Some short circuiting "magic"
+        translate(x, y);
+        scale(scaler);
+        beginShape();
+            vertex(-Tile.width * 0.5, -Tile.height);  // Top Left
+            vertex(Tile.width * 0.5, -Tile.height);   // Top Right
+            vertex(Tile.width, 0);                    // Middle Right
+            vertex(Tile.width * 0.5, Tile.height);    // Bottom Right
+            vertex(-Tile.width * 0.5, Tile.height);   // Bottom Left
+            vertex(-Tile.width, 0);                   // Middle Left
+        endShape(CLOSE);
+    }
+
     draw() {
 
         // Placement
